41368a619a7543b52a0df91f5eebd141ed6ad811
mitAzim <azim@masterinfotech.com>
2023-03-15 16:56:52 +0530

    TravellerAddOn page added

--- src/components/Routes/AllRoutes.js
+++ src/components/Routes/AllRoutes.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import Home from '../Home/Home';
-import SignIn from '../SignIn/SignIn';
-import SignUp from '../Signup/SignUp';
-import {Routes, Route} from 'react-router-dom';
-import Hotels from '../Hotels/Hotels';
-import PrivateRoute from './PrivateRoute';
-import TopDestinations from '../TopDestinations/TopDestinations';
-import BookSuccess from '../Booking/BookSuccess';
-import ContactUs from '../ContactUs.js/ContactUs';
-import OfferDeals from '../OfferDeals/OfferDeals';
-import TravellerAddOn from './../TravellerAddOn/TravellerAddOn';
-import TravellerMain from './../TravellerAddOn/TravellerMain';
+import React from "react";
+import Home from "../Home/Home";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../Signup/SignUp";
+import { Routes, Route } from "react-router-dom";
+import Hotels from "../Hotels/Hotels";
+import PrivateRoute from "./PrivateRoute";
+import TopDestinations from "../TopDestinations/TopDestinations";
+import BookSuccess from "../Booking/BookSuccess";
+import ContactUs from "../ContactUs.js/ContactUs";
+import OfferDeals from "../OfferDeals/OfferDeals";
+import TravellerAddOn from "./../TravellerAddOn/TravellerAddOn";
+import TravellerMain from "./../TravellerAddOn/TravellerMain";
+import Payment from "./../Payment/Payment";
+import Privacy from './../Privacy/Privacy';
 
 const AllRoutes = () => {
   return (
     <>
-        <Routes> 
-            <Route path='/' element={<PrivateRoute/>}>
-                <Route  index  element={<Home/>}></Route>
-                <Route  path='/hotels' element={<Hotels/>}></Route>
-                <Route  path='/top-destinations' element={<TopDestinations/>}></Route>
-                <Route  path='/book-success' element={<BookSuccess/>}></Route>
-                <Route  path='/contact' element={<ContactUs/>}></Route>
-                <Route  path='/deals-offers' element={<OfferDeals/>}></Route>
-            <Route  path='/TravellerAddOn' element={<TravellerMain/>}></Route>
-            </Route>
-            <Route  path='/sign-up' element={<SignUp/>}></Route>
-            <Route  path='/sign-in' element={<SignIn/>}></Route>
+      <Routes>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/hotels" element={<Hotels />}></Route>
+          <Route path="/top-destinations" element={<TopDestinations />}></Route>
+          <Route path="/book-success" element={<BookSuccess />}></Route>
+          <Route path="/contact" element={<ContactUs />}></Route>
+          <Route path="/deals-offers" element={<OfferDeals />}></Route>
+          <Route path="/TravellerAddOn" element={<TravellerMain />}></Route>
+          <Route path="/Payment" element={<Payment />}></Route>
+          <Route path="/Privacy" element={<Privacy />}></Route>
+        </Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
       </Routes>
-     
-    
     </>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
