381c63420d85aaac86d05a75e9f405a0ba70e2f1
Deepjaz <114072773+Deepjaz@users.noreply.github.com>
2023-03-29 10:57:52 +0530

    resolve the checkout error

--- src/components/Flights/FlightCheckout/FlightCheckout.js
+++ src/components/Flights/FlightCheckout/FlightCheckout.js
@@ -18,7 +18,6 @@ const FlightCheckout = () => {
   const month = date.toLocaleDateString('en-us' , {month : 'long'})
   const time = date.toLocaleTimeString('en-us' , {timeStyle : "short"})
   const depTime = departureDate.toLocaleTimeString('en-us' , {timeStyle : "short"})
-
   // const timebetweenFlight = time  - depTime
   const getDate = date.getDate();
   console.log('dateStringdateStringdateString' ,day , month , getDate ,time ,depTime )
@@ -87,7 +86,8 @@ const FlightCheckout = () => {
                               <div className="makeFlex gap-x-10">
                                 <div className="makeFlex time-info-ui">
                                   <span className="fontSize14 blackFont">
-                                    {time}
+                                    {depTime}
+                                    
                                   </span>
                                   <span className="layoverCircle"></span>
                                 </div>
@@ -107,7 +107,7 @@ const FlightCheckout = () => {
                               <div className="makeFlex gap-x-10 overideBg">
                                 <div className="makeFlex time-info-ui">
                                   <span className="fontSize14 blackFont">
-                                    {depTime}
+                                  {time}
                                   </span>
                                   <span className="layoverCircle"></span>
                                 </div>
@@ -134,7 +134,7 @@ const FlightCheckout = () => {
                                   ADULT
                                 </span>
                                 <span className="fontSize12 blackFont">
-                                  {val.co2Emissions[0].carryWeight} {val.co2Emissions[0].weightUnit} (1 piece only)
+                                  {val.co2Emissions[0].carryWeight[0]} {val.co2Emissions[0].weightUnit} (1 piece only)
                                 </span>
                                 <span className="fontSize12 blackFont">
                                 {val.co2Emissions[0].cabin}
@@ -144,276 +144,7 @@ const FlightCheckout = () => {
                           </div>
                         </div>
                       </div>
-                      {/* <div className="mmtConnectReviewInfo">
-                        <ul>
-                          <li>
-                            <p>
-                              <b>
-                                <font color="#cf8100">Change of planes</font>
-                              </b>
-                              <br />
-                              <b>5h 15m</b> Layover in Jaipur
-                            </p>
-                          </li>
-                        </ul>
-                      </div> */}
-                      {/* <div className="flightItenaryWrap">
-                        <div className="flightItenaryHdr">
-                          <div className="makeFlex gap-x-10">
-                            <span className="bgProperties icon24"></span>
-                            <p className="makeFlex gap-x-10">
-                              <span className="fontSize14 boldFont">
-                                AirAsia
-                              </span>
-                              <span className="fontSize14">I5 830</span>
-                            </p>
-                          </div>
-                          <div className="makeFlex">
-                            <div className="makeFlex hrtlCenter">
-                              <span className="fontSize14">
-                                Economy &gt;
-                                <font color="#249995">
-                                  <b>Economy</b>
-                                </font>
-                              </span>
-                              <span className="bgProperties icon16 appendLeft5 appendTop2"></span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flightItenary">
-                          <div className="flexOne">
-                            <div className="itenaryLeft">
-                              <div className="makeFlex gap-x-10">
-                                <div className="makeFlex time-info-ui">
-                                  <span className="fontSize14 blackFont">
-                                    21:45
-                                  </span>
-                                  <span className="layoverCircle"></span>
-                                </div>
-                                <div>
-                                  <span className="fontSize14 blackFont">
-                                    Jaipur{" "}
-                                  </span>
-                                  <span className="fontSize14">
-                                    . Jaipur Airport
-                                  </span>
-                                </div>
-                              </div>
-                              <div className="layover">
-                                <span className="fontSize14">1h 0m</span>
-                              </div>
-                              <div className="makeFlex gap-x-10 overideBg">
-                                <div className="makeFlex time-info-ui">
-                                  <span className="fontSize14 blackFont">
-                                    22:45
-                                  </span>
-                                  <span className="layoverCircle"></span>
-                                </div>
-                                <div>
-                                  <span className="fontSize14 blackFont">
-                                    New Delhi{" "}
-                                  </span>
-                                  <span className="fontSize14">
-                                    . Indira Gandhi International Airport,
-                                    Terminal 3
-                                  </span>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="itenaryRight">
-                            <ul className="itenaryList">
-                              <li>
-                                <span className="fontSize12">Baggage</span>
-                                <span className="fontSize12">Check-in</span>
-                                <span className="fontSize12">Cabin</span>
-                              </li>
-                              <li>
-                                <span className="fontSize12 blackFont">
-                                  ADULT
-                                </span>
-                                <span className="fontSize12 blackFont">
-                                  15 Kgs (1 piece only)
-                                </span>
-                                <span className="fontSize12 blackFont">
-                                  7 Kgs (1 piece only)
-                                </span>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </div> */}
-                    </section>
-                    {/* <div className="baggageTag">
-                      <div className="baggageContent">
-                        <span className="addBaggageImg bgProperties appendRight10"></span>
-                        <p className="fontSize12 boldFont">
-                          Got excess luggage? Don't stress, buy extra check-in
-                          baggage allowance at fab rates!
-                        </p>
-                      </div>
-                      <div>
-                        <button type="button" className="addBtn">
-                          +ADD
-                        </button>
-                      </div>
-                    </div> */}
-                  </div>
-                  {/* <div className="reviewUpgradeFare appendTop20">
-                    <p className="blackText fontSize18 blackFont appendBottom5"></p>
-                    <p className="appendBottom15"></p>
-                    <div className="make_flex"></div>
-                  </div> */}
-                  {/* <div className="appendTop20">
-                    <section className="refundSection">
-                      <div>
-                        <div className="refundHdr">
-                          <div className="makeFlex hrtlCenter">
-                            <h3 className="fontSize16 blackFont">
-                              <font color="#000000">
-                                Cancellation Refund Policy
-                              </font>
-                            </h3>
-                          </div>
-                          <div>
-                            <p className="fontSize14 darkText textRight">
-                              <span className="linkText appendLeft3">
-                                View Policy
-                              </span>
-                            </p>
-                          </div>
-                        </div>
-                        <div className="flightDetails reviewCanPolicyWrapper">
-                          <div className="cancSecWrap">
-                            <p className="flightDetailsInfo makeFlex hrtlCenter">
-                              <span className="bgProperties icon24"></span>
-                              <span className="blackFont darkText appendLeft10">
-                                BOM-DEL
-                              </span>
-                            </p>
-                            <div className="timeLineDetailsInfo makeFlex">
-                              <div className="cancInfoLeft">
-                                <p className="appendBottom20">
-                                  Cancellation Penalty :
-                                </p>
-                                <p>Cancel Between (IST) :</p>
-                              </div>
-                              <div className="flexOne">
-                                <div className="makeFlex">
-                                  <span className="cancPriceInfo fontSize16">
-                                    ₹ 3,300
-                                  </span>
-                                  <span className="cancPriceInfo fontSize16">
-                                    ₹ 3,800
-                                  </span>
-                                  <span className="cancPriceInfo fontSize16">
-                                    ₹ 4,567
-                                  </span>
-                                </div>
-                                <p className="cancGradline"></p>
-                                <div className="cancTimeline">
-                                  <div className="cancTimeNode">
-                                    <p className="blackFont">Now</p>
-                                  </div>
-                                  <div className="cancTimeNode">
-                                    <p className="blackFont">7 May</p>
-                                    <p className="fontSize12 boldFont">14:45</p>
-                                  </div>
-                                  <div className="cancTimeNode">
-                                    <p className="blackFont">10 May</p>
-                                    <p className="fontSize12 boldFont">12:45</p>
-                                  </div>
-                                  <div className="cancTimeNode">
-                                    <p className="blackFont">10 May</p>
-                                    <p className="fontSize12 boldFont">14:45</p>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="refundUpgradeCard appendBottom10">
-                          <div className="makeFlex hrtlCenter">
-                            <span className="appendRight10">
-                              <span className="icon24 bgProperties"></span>
-                            </span>
-                            <span className="boldFont fontSize12">
-                              More fares with flexible refund &amp; date change
-                              policy available!
-                            </span>
-                          </div>
-                          <div>
-                            <span className="linkText boldFont fontSize12 padding5">
-                              UPGRADE
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </section>
-                  </div> */}
-                  {/* <div className="appendTop20">
-                    <div className="appendTop20">
-                      <section className="refundSection">
-                        <h3 className="fontSize16 blackFont appendBottom20">
-                          Unsure of your travel plans?
-                        </h3>
-                        <div id="0">
-                          <div
-                            className="unsureBanner makeFlex appendBottom10"
-                            id="0"
-                          >
-                            <span className="customCheckbox appendTop2">
-                              <input
-                                className="pointer"
-                                type="checkbox"
-                                id="zc-fdc-checkbox"
-                                name="<font color='#3023AE'><b>Add Zero Cancellation</b></font>"
-                                readonly=""
-                              />
-                              <span className="box">
-                                <span className="check"></span>
-                              </span>
-                            </span>
-                            <div className="makeFlex flexOne column">
-                              <div className="makeFlex spaceBetween flexOne appendLeft10">
-                                <div className="flexOne">
-                                  <div className="makeFlex appendBottom5 hrtlCenter">
-                                    <span className="boldFont appendRight10 inlineB">
-                                      <font color="#FFFFFF">
-                                        <b>Add Zero Cancellation</b>
-                                      </font>
-                                    </span>
-                                  </div>
-                                  <div className="fontSize12 makeFlex">
-                                    <p className="darkText">
-                                      <b>Get refund of up to ₹ 4,567</b> in case
-                                      of a cancellation up to 24hrs before
-                                      departure
-                                    </p>
-                                    <span className="tncText">
-                                      <font color="#0091FF">View T&amp;C</font>
-                                    </span>
-                                  </div>
-                                </div>
-                                <div className="unsureBannerRight">
-                                  <span className="bgProperties icon20"></span>
-                                  <p className="blackFont blackText">₹ 1,549</p>
-                                </div>
-                              </div>
-                              <p className="unsurePersuasion fontSize11">
-                                <span>
-                                  <font color="#FFFFFF">
-                                    6,480 customers purchased this on your route
-                                    <font></font>
-                                  </font>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </section>
-                    </div>
-                  </div> */}
+                      
                 </div>
               </div>
               <div class="pageRightContainer customScroll">
@@ -439,40 +170,7 @@ const FlightCheckout = () => {
                       }
                       
                     </div>
-                    {/* <div class="fareTypeWrap">
-                      <div class="fareRow">
-                        <div class="makeFlex hrtlCenter pointer flexOne">
-                          <span class="appendRight10 appendTop5">
-                            <span class="iconPlusImg bgProperties"></span>
-                          </span>
-                          <span class="fareHeader">Taxes and Surcharges</span>
-                        </div>
-                        <span class="fontSize14 darkText">₹ 653</span>
-                      </div>
-                    </div> */}
-                    {/* <div class="fareTypeWrap">
-                      <div class="fareRow">
-                        <div class="makeFlex hrtlCenter pointer flexOne">
-                          <span class="appendRight10 appendTop5">
-                            <span class="iconMinusImg bgProperties"></span>
-                          </span>
-                          <span class="fareHeader">Other Services</span>
-                        </div>
-                      </div>
-                      <div class="fareContentWrap">
-                        <div class="fareSubList">
-                          <p class="fareRow appendTop5">
-                            <span class="makeFlex bottom">
-                              <span class="fontSize14 darkText">Charity</span>
-                              <span class="charityAddTxt">REMOVE</span>
-                            </span>
-                            <span class="fontSize14 darkText">
-                              <span>₹ 10</span>
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    </div> */}
+                    
                     <div class="fareFooter">
                       <p class="fareRow">
                         <span class="fontSize16 blackFont">Total Amount</span>
